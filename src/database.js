93517e22c6951be7ac3601b789b33d7be10cf9c7
Juanb Solari <solari.jb@gmail.com>
2022-10-20 14:58:24 -0300

    MongoAtlas connected

--- src/database.js
+++ src/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 const {MONGODB_URI} = process.env
-
-mongoose.connect(MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>{console.log('Database connected: OK')})
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{console.log('Database connected: OK')})
